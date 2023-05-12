9d229065997d82f1f7f3d45aec6e896d10a2da50
[Abdul Suboor] <[suboorsa15@gmail.com]>
2023-05-12 10:07:10 +0530

    yo

--- client/src/App.js
+++ client/src/App.js
@@ -15,13 +15,13 @@ function App() {
   return (
     <UserContextProvider>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="https://blogs-app-ulti.onrender.com/" element={<Layout />}>
           <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="https://blogs-app-ulti.onrender.com/login" element={<LoginPage />} />
+          <Route path="https://blogs-app-ulti.onrender.com/register" element={<RegisterPage />} />
+          <Route path="https://blogs-app-ulti.onrender.com/create" element={<CreatePost />} />
+          <Route path="https://blogs-app-ulti.onrender.com/post/:id" element={<PostPage />} />
+          <Route path="https://blogs-app-ulti.onrender.com/edit/:id" element={<EditPost />} />
         </Route>
       </Routes>
     </UserContextProvider>
